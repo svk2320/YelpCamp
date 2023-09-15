3575a412333ca0617748f3b825b6322c9992b1cc
vasanth2320 <vasanthkumars2025@gmail.com>
2023-09-15 17:31:12 +0530

    removed unwanted files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline mx-0.5">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline mx-3">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App;
