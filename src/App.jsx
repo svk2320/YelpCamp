6a245fe1369f24ac866be38326835ec715839b2a
vasanth2320 <vasanthkumars2025@gmail.com>
2023-09-15 18:03:08 +0530

    modified home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Home from './pages/home/home.page';
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline mx-0.5">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline mx-3">Hello world!</h1>
+      <Home />
     </div>
   )
 }
